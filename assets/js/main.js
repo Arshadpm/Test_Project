5b2ee8afb77f91492f03eeffa52e9bb814e64098
Arshadpm <arshadpmuhammed@gmail.com>
2026-01-11 13:30:12 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,8 @@ function toggleMobileMenu() {
   const hamburgerIcon = document.getElementById("hamburgerIcon");
   const closeIcon = document.getElementById("closeIcon");
 
-  // Toggle menu visibility
   mobileMenu.classList.toggle("hidden");
 
-  // Toggle icons
   hamburgerIcon.classList.toggle("hidden");
   closeIcon.classList.toggle("hidden");
 }
